8583643f70e56e67d3540df4f9a5be3f23b80a58
xuning <m18510527643@163.com>
2018-08-08 10:35:39 +0800

    第二次提交

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MyInject-demo
 绑定Android Views和回调方法，Bind Android views and callbacks to fields and methods.
+利用反射加注解的原理，实现Activity或者fragment中的布局注入，以及使用注解的方式实现View的findViewById，
+和onClickListener方法，具体使用非常简单，见实例MainActivity（Fragment同理）
--- app/src/main/java/com/inject/xn/MainActivity.java
+++ app/src/main/java/com/inject/xn/MainActivity.java
@@ -3,13 +3,35 @@ package com.inject.xn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.xn.dlib.XnClickView;
+import com.xn.dlib.XnContentView;
+import com.xn.dlib.XnFindView;
+
+@XnContentView(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @XnFindView(R.id.tv3)
+    private TextView tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tv3.setText("tv33333333333333");
+    }
+    @XnClickView({R.id.tv1,R.id.tv2})
+    public void clickView(View view){
+        switch (view.getId()){
+            case R.id.tv1:
+                Toast.makeText(this, "TV1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv2:
+                Toast.makeText(this, "TV2", Toast.LENGTH_SHORT).show();
+                break;
 
+        }
     }
 }
